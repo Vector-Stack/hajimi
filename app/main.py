a8a56befe7de75e4dc4f3a60ab99c215ae1c7d79
Vector-Stack <59189393+Vector-Stack@users.noreply.github.com>
2025-07-13 19:28:57 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
